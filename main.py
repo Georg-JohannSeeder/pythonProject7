43fab219cdb0d38df01851eec4027dc9c5ccf236
Georg-JohannSeeder <115535288+Georg-JohannSeeder@users.noreply.github.com>
2023-02-15 11:36:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,19 @@ font = pygame.font.Font(None, 30)
 text = font.render("Hello PyGame", True, [0,0,0])
 screen.blit(text, [200,200])
 #Lisame pildid
-bg = pygame.image.load("bg_shop.png")
-bg = pygame.image.load("seller.png")
-bg = pygame.image.load("chat.png")
-screen.blit(bg,[0,0])   
+bg = pygame.image.load("imgf/bg_shop.jpg")
+screen.blit(bg,[0,0])
+sel = pygame.image.load("imgf/seller.png")
+sel = pygame.transform.scale(sel,[250,300])
+screen.blit(sel,[60,150])
+cht = pygame.image.load("imgf/chat.png")
+cht = pygame.trasnform.scale(cht,[250,200])
+screen.blit(cht,[200,75])
+font = pygame.font.Font(pygame.font.match_font('Arial'), 25)
+text = font.render("Georg-Johann Seeder", True, [255, 255, 255])
+text_width = text.get_rect().width
+text_height = text.get_rect().height
+screen.blit(text, [225, 150])
 pygame.display.flip()
 running = True
 while running:
